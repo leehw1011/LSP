73ddc2f6419e4a45b5e2eda98497c45b51d2c7ba
leehw1011 <leehw1011@naver.com>
2021-04-17 14:22:42 +0900

    myls -l

--- practice3/_myls.c
+++ practice3/_myls.c
@@ -71,13 +71,24 @@ int main(int argc, char **argv)
 						closedir(dp);
 						break;
 					case 'l':
-						printf("option : l\n");
+						if((dp=opendir(dir))==NULL){
+							perror(dir);
+						}
+
+						while((d=readdir(dp))!=NULL){
+							sprintf(path,"%s/%s",dir,d->d_name);
+							if(lstat(path,&st)<0)
+								perror(path);
+							printStat(path,d->d_name,&st);
+						}
+						closedir(dp);
 						break;
 					case 't':
 						printf("option : t\n");
 						break;
 				}
 			}
+			exit(0);
 		}
 		//filename이 주어진 경우
 		else{
@@ -103,7 +114,6 @@ int main(int argc, char **argv)
 					}
 				}
 				printf("\n");
-				//noneOpt(dp);
 				closedir(dp);
 				exit(0);
 			}
@@ -113,11 +123,11 @@ int main(int argc, char **argv)
 
 //파일 상태 정보를 출력
 void printStat(char *pathname, char *file, struct stat *st){
-	printf("%5d ",st->st_blocks);
+	//printf("%5d ",st->st_blocks);
 	printf("%c%s ",type(st->st_mode),perm(st->st_mode));
-	printf("%3d ",st->st_nlink);
+	printf("%2d ",st->st_nlink);
 	printf("%s %s ",getpwuid(st->st_uid)->pw_name, getgrgid(st->st_gid)->gr_name);
-	printf("%9d ", st->st_size);
+	printf("%6d ", st->st_size);
 	printf("%.12s ",ctime(&st->st_mtime)+4);
 	printf("%s\n",file);
 }
