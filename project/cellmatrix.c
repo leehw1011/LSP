86aace31aa6afba239d48efd555a6f72e3c442cd
leehw1011 <leehw1011@naver.com>
2021-06-04 18:05:45 +0900

    complete

--- project/cellmatrix.c
+++ project/cellmatrix.c
@@ -4,7 +4,7 @@
 #include <sys/wait.h>
 #include <pthread.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
 
 typedef struct MultipleArg{
 	int start;
@@ -15,24 +15,20 @@ int** matrix;
 int** output;
 int m,n;	// matrix size
 
-void readInput(FILE* fp){ //, int** matrix, int* m, int* n){
+void readInput(FILE* fp){
 	// 입력된 파일의 행, 열 계산(m x n matrix)
+
 	rewind(fp);
 	char ch;
-	//*m = 0, *n = 0;
 	m = 0 ,n = 0;
 	while((ch = fgetc(fp))!=EOF){
-		if(ch=='0' || ch=='1') n++;// (*n)++;
-		else if(ch=='\n') m++; //(*m)++;
+		if(ch=='0' || ch=='1') n++;
+		else if(ch=='\n') m++;
 	}
 
 	m = m+1;
 	n /= m;
 	
-	//*m = *m+1;
-	//*n /= *m;
-	printf("행 : %d, 열: %d\n",m,n);
-
 	// 2차원 배열 동적할당
 	matrix = (int**) malloc(sizeof(int*) * m);
 	for(int i=0;i<m;i++){
@@ -46,17 +42,16 @@ void readInput(FILE* fp){ //, int** matrix, int* m, int* n){
 			*(*(matrix+j)+i) = atoi(&ch);
 			i++;
 		}
-		if(i==n){
+		if(ch=='\n'){
 			j++;
 			i=0;
 		}
 	}
 	return;
-	//return matrix;
 }
 
 // gen_n.matrix, output.matrix 파일을 만드는 함수
-void mkfile(int gen){	//(int** matrix, int gen, int m, int n){
+void mkfile(int gen){
 
 	FILE* fp;
 	char filename1[30] = "gen_";
@@ -68,8 +63,6 @@ void mkfile(int gen){	//(int** matrix, int gen, int m, int n){
 		strcat(filename1,num);
 		strcat(filename1,filename2);
 	}
-
-	//printf("%s\n",filename1);
 
 	if((fp = fopen(filename1,"w+"))==NULL){
 		printf("file open error\n");
@@ -88,7 +81,7 @@ void mkfile(int gen){	//(int** matrix, int gen, int m, int n){
 }
 
 // matrix[x][y]의 살아있는 이웃 수를 구하는 함수
-int getneighbor(int x, int y){	//(int** matrix, int m, int n, int x, int y){
+int getneighbor(int x, int y){
 	int neighborCell=0;
 	int dx[8] = {-1,-1,-1,0,0,1,1,1};
 	int dy[8] = {-1,0,1,-1,1,-1,0,1};
@@ -100,7 +93,7 @@ int getneighbor(int x, int y){	//(int** matrix, int m, int n, int x, int y){
 	return neighborCell;
 }
 
-void run(int i, int j){	//(int **matrix,int **output, int m, int n, int i, int j){
+void run(int i, int j){
 
 	int neighborCell;
 	// 해당 세포가 살아있는 경우
@@ -125,27 +118,18 @@ void run(int i, int j){	//(int **matrix,int **output, int m, int n, int i, int j
 // 각 스레드가 실행할 함수 
 void *run_thread(void *myargs){
 
+	pthread_t id;
 	args *data = (args *)myargs;
 
 	int start = data->start;
 	int end = data->end;
 
-	printf("start : %d, end : %d\n\n",start,end);
+	printf("%ld ",pthread_self());
 	for(int i=start;i<end;i++){
 		for(int j=0;j<n;j++){
 			run(i,j);
-			printf("%d ",output[i][j]);
 		}
-		printf("\t%d열\n",i);
 	}
-
-	printf("\n");
-	/*for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                        printf("%d ",output[i][j]);
-                }
-                printf("\n");
-        }*/
 
 	pthread_exit(NULL);
 	return NULL;
@@ -153,18 +137,12 @@ void *run_thread(void *myargs){
 
 void seq(FILE* fp, int gennum){
 
-	//int **matrix;
-	//int **output;
-	//int neighborCell;	// 살아있는 이웃 세포의 수
-	//int m,n;	// matrix size
-	clock_t start, end;
-	double duration;
+	struct timeval start,end;
+	double diff;
 
 	// 시작 시간 체크
-	start = clock();
-
+	gettimeofday(&start,NULL);
 	// 입력된 파일 읽기
-	//matrix = readInput(fp,matrix,&m,&n);
 	readInput(fp);
 
 	// gennum만큼 세대 진행, gen_n.matrix 파일 생성
@@ -174,6 +152,7 @@ void seq(FILE* fp, int gennum){
                 	output[i] = (int*)malloc(sizeof(int)*n);
         	}
 
+		// 세대 진행
 		for(int i=0;i<m;i++){
 			for(int j=0;j<n;j++){
 				run(i,j);
@@ -204,28 +183,30 @@ void seq(FILE* fp, int gennum){
         free(matrix);
 
 	// 종료 시간 체크
-	end = clock();
+	gettimeofday(&end,NULL);
 
 	// 총 수행시간 출력
-	duration = (double)(end-start);
-	printf("수행 시간 : %lf (ms)\n",duration);
+	diff = end.tv_usec - start.tv_usec;
+	printf("수행 시간 : %lf (ms)\n\n",diff);
 	return;
 }
 
 void process(FILE* fp, int childnum, int gennum){
 
-	//int **matrix;
-	//int **output;
-	//int neighborCell;
-	//int m,n;
-	clock_t start, end;
-	double duration;
+	struct timeval start, end;
+	double diff;
 	pid_t pids[childnum], pid;
 	int status;
+	char buffer[m*n];
+	int fd[2];
+	if(pipe(fd)==-1){
+		printf("pipe error\n");
+		exit(1);
+	}
 	// 시작 시간 체크
-	//
+	gettimeofday(&start,NULL);
+
 	// 입력된 파일 읽기
-	//matrix = readInput(fp,matrix,&m,&n);
 	readInput(fp);
 
 	// 한 프로세스가 몇 개의 행을 실행할지 구하기!
@@ -233,21 +214,19 @@ void process(FILE* fp, int childnum, int gennum){
 	int remain = m%childnum;
 	int sum=0;
 	int per_process[childnum];
-	int offset[childnum];
+	int offset[childnum+1];
 
-	for(int i=0;i<childnum;i++){
+	for(int i=0;i<=childnum;i++){
 		if(i<remain){per_process[i] = div+1;}
 		else per_process[i] = div;
 		offset[i] = sum;
 		sum+=per_process[i];
-		//printf("offset[%d] = %d ",i,offset[i]);
-		//printf("per_process[%d] : %d ",i,per_process[i]);
 	}
-	printf("\n");
 	
 	// 각각의 프로세스에서 만들어지는 output을 어떻게 처리하지 !?
 	// 자식 프로세스 생성(childnum만큼 반복하며 fork())
 	for(int gen=0;gen<gennum;gen++){
+		printf("\n-- %d세대 진행에 사용한 프로세스 ID --\n",gen+1);
 		output = (int**) malloc(sizeof(int*) * m);
 		for(int i=0;i<m;i++){
 			output[i] = (int*)malloc(sizeof(int)*n);
@@ -259,53 +238,74 @@ void process(FILE* fp, int childnum, int gennum){
 				exit(1);
 			}
 			else if(pids[processnum]==0){	// child(processnum번째 자식?)
+				int a=0;
 				for(int i=offset[processnum];i<offset[processnum+1];i++){
 					for(int j=0;j<n;j++){
 						run(i,j);
+						buffer[a] = output[i][j];
+						a++;
 					}
 				}
-				//printf("I am a child, %d\n",processnum);
-				exit(0);
+				write(fd[1],buffer,m*n);
+				printf("%d ",getpid());
+				
+				exit(0);	// child process 종료
 			}
 			else{				// parent
-				if(wait(&status)!=pids[processnum]){
-					printf("wait error\n");
-					exit(1);
+				read(fd[0],buffer,m*n);
+				int a=0;
+				for(int i=offset[processnum];i<offset[processnum+1];i++){
+					for(int j=0;j<n;j++){
+						output[i][j] = buffer[a];
+						a++;
+					}
 				}
-				//printf("I am a parent\n");
 			}
 		}
-		// 여기서! 각 세대마다 gen_n.matrix 파일을 만들어야하는데 ...
-		//
-		printf("-----%d세대------\n",gen);
+		while(wait((int *)0) != -1);
+
+		// 각 세대마다 gen_n.matrix 파일 만들기
 		for(int i=0;i<m;i++){
 			for(int j=0;j<n;j++){
-				printf("%d ",output[i][j]);
+				matrix[i][j]=output[i][j];
 			}
-			printf("\n");
 		}
+
+		mkfile(gen+1);
 	}
 
-	// 각 프로세스에서 세대 진행, gen_n.matrix 파일 생성
-	// pid로 프로세스 체크??
-	//
 	// output.matrix 파일 생성
-	//
+	mkfile(-1);
+
+	for(int i=0;i<m;i++){
+		free(output[i]);
+        }
+        free(output);
+
+        for(int i=0;i<m;i++){
+                free(matrix[i]);
+        }
+        free(matrix);
+	
 	// 종료 시간 체크
-	//
+	gettimeofday(&end,NULL);
+	
 	// 총 수행시간 출력
+	diff = end.tv_usec - start.tv_usec;
+	printf("\n수행 시간 : %lf (ms)\n\n",diff);
 	return;
 }
+
 void thread(FILE* fp, int total_thread, int gennum){
 	
 	args args_array[total_thread];
-        clock_t start, end;
-        double duration;
+        struct timeval start, end;
+        double diff;
         pthread_t tid[total_thread];
 	int status;
 
 	// 시작 시간 체크
-        start = clock();
+        gettimeofday(&start,NULL);
 
         // 입력된 파일 읽기
 	readInput(fp);
@@ -322,12 +322,11 @@ void thread(FILE* fp, int total_thread, int gennum){
                 else per_thread[i] = div;
                 offset[i] = sum;
                 sum+=per_thread[i];
-		printf("%d ",offset[i]);
         }
-        printf("\n");
 
         // 스레드 생성(total_thread만큼 반복하며 pthread_create())
         for(int gen=0;gen<gennum;gen++){
+		printf("\n-- %d 세대 진행에 사용한 thread ID --\n",gen);
                 output = (int**) malloc(sizeof(int*) * m);
                 for(int i=0;i<m;i++){
                         output[i] = (int*)malloc(sizeof(int)*n);
@@ -346,34 +345,35 @@ void thread(FILE* fp, int total_thread, int gennum){
 		for(int threadnum=0;threadnum<total_thread;threadnum++){
 			pthread_join(tid[threadnum],(void*)&status);
 		}
+
                 // 각 세대마다 gen_n.matrix 파일 만들기
-                //
                 for(int i=0;i<m;i++){
 			for(int j=0;j<n;j++){
 				matrix[i][j] = output[i][j];
 			}
 		}
-
 		mkfile(gen+1);
-		printf("-----%d세대------\n",gen+1);
-                for(int i=0;i<m;i++){
-                        for(int j=0;j<n;j++){
-                                printf("%d ",output[i][j]);
-                        }
-                        printf("\n");
-                }
-
         }
 
         // output.matrix 파일 생성
 	mkfile(-1);
-        //
+        
+	for(int i=0;i<m;i++){
+                free(output[i]);
+        }
+        free(output);
+
+        for(int i=0;i<m;i++){
+                free(matrix[i]);
+        }
+        free(matrix);
+
 	// 종료 시간 체크
-	end = clock();
+	gettimeofday(&end,NULL);
 
 	// 총 수행시간 출력
-	duration = (double)(end-start);
-	printf("수행 시간 : %lf (ms)\n",duration);
+	diff = end.tv_usec - start.tv_usec;
+	printf("\n수행 시간 : %lf (ms)\n\n",diff);
 	return;
 }
 
